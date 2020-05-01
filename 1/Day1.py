24d92556dfa970ca8899f046cc68e3d9f0e130f2
Dan Krecoum <57428694+dankrecoum@users.noreply.github.com>
2020-05-01 15:06:35 -0400

    Update Day1.py
    Fixed a bug in 'add_up_to_bonus'

--- 1/Day1.py
+++ 1/Day1.py
@@ -44,7 +44,7 @@ def add_up_to_bonus(array, k):
     """
     add_up = False
     for i in range(len(array)):
-        if (k - array[i]) in array[:(i+1)]:
+        if (k - array[i]) in array[:(i+1)] and array.count(array[i]) !=1:
             return not add_up
     return add_up
 
