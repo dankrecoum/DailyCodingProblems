9aa5ed8402cd11e457b11f9fb80ec12d4375064e
Dan Krecoum <57428694+dankrecoum@users.noreply.github.com>
2020-05-23 17:51:40 -0400

    Update Day1.py

--- 1/Day1.py
+++ 1/Day1.py
@@ -37,14 +37,14 @@ def add_up_to_bonus(array, k):
 
     Args :
         array (list): The array to be processed
-        k (int): The integer that that can be the sum of two numbers of the array
+        k (int): The integer that can be the sum of two numbers of the array
 
     Returns :
         add_up (bool): The boolean value that is True when two numbers of the array add up to k
     """
     add_up = False
     for i in range(len(array)):
-        if (k - array[i]) in array[:(i+1)]:
+        if (k - array[i]) in array[(i+1):]:
             if not ((array.count(array[i]) == 1) and array[i]*2 == k):
                 return not add_up
     return add_up
